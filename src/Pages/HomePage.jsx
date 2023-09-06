ea80369f494e92c88ea46cf2625d961fedd0b9b6
Dhawal Bisht <dhawalbisht4543@gmail.com>
2023-09-06 10:48:40 +0530

    homepage updated

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+export default function HomePage() {
+    return (
+        <>
+            <Navbar />
+            <div className="caption">
+                <h1>Believe and Build the link!</h1>
+            </div>
+        </>
+    )
+}
